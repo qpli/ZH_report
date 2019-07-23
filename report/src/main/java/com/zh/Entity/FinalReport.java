b5416d93fc4bbc7d6ef5ea3b4f48444c91775f0b
lisq0911 <1140169192@qq.com>
2019-07-23 10:44:37 +0800

    添加static

---
+++ report/src/main/java/com/zh/Entity/FinalReport.java
@@ -0,0 +1,211 @@
+package com.zh.Entity;
+
+public class FinalReport {
+    Integer finalId;
+    Integer delFlag;
+    Integer reportId;
+    String col1;
+    String col2;
+    String col3;
+    String col4;
+    String col5;
+    String col6;
+    String col7;
+    String col8;
+    String col9;
+    String col10;
+    String col11;
+    String col12;
+    String col13;
+    String col14;
+    String col15;
+    String col16;
+    String col17;
+    String col18;
+    String col19;
+    String col20;
+
+    public Integer getFinalId() {
+        return finalId;
+    }
+
+    public void setFinalId(Integer finalId) {
+        this.finalId = finalId;
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public Integer getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(Integer reportId) {
+        this.reportId = reportId;
+    }
+
+    public String getCol1() {
+        return col1;
+    }
+
+    public void setCol1(String col1) {
+        this.col1 = col1;
+    }
+
+    public String getCol2() {
+        return col2;
+    }
+
+    public void setCol2(String col2) {
+        this.col2 = col2;
+    }
+
+    public String getCol3() {
+        return col3;
+    }
+
+    public void setCol3(String col3) {
+        this.col3 = col3;
+    }
+
+    public String getCol4() {
+        return col4;
+    }
+
+    public void setCol4(String col4) {
+        this.col4 = col4;
+    }
+
+    public String getCol5() {
+        return col5;
+    }
+
+    public void setCol5(String col5) {
+        this.col5 = col5;
+    }
+
+    public String getCol6() {
+        return col6;
+    }
+
+    public void setCol6(String col6) {
+        this.col6 = col6;
+    }
+
+    public String getCol7() {
+        return col7;
+    }
+
+    public void setCol7(String col7) {
+        this.col7 = col7;
+    }
+
+    public String getCol8() {
+        return col8;
+    }
+
+    public void setCol8(String col8) {
+        this.col8 = col8;
+    }
+
+    public String getCol9() {
+        return col9;
+    }
+
+    public void setCol9(String col9) {
+        this.col9 = col9;
+    }
+
+    public String getCol10() {
+        return col10;
+    }
+
+    public void setCol10(String col10) {
+        this.col10 = col10;
+    }
+
+    public String getCol11() {
+        return col11;
+    }
+
+    public void setCol11(String col11) {
+        this.col11 = col11;
+    }
+
+    public String getCol12() {
+        return col12;
+    }
+
+    public void setCol12(String col12) {
+        this.col12 = col12;
+    }
+
+    public String getCol13() {
+        return col13;
+    }
+
+    public void setCol13(String col13) {
+        this.col13 = col13;
+    }
+
+    public String getCol14() {
+        return col14;
+    }
+
+    public void setCol14(String col14) {
+        this.col14 = col14;
+    }
+
+    public String getCol15() {
+        return col15;
+    }
+
+    public void setCol15(String col15) {
+        this.col15 = col15;
+    }
+
+    public String getCol16() {
+        return col16;
+    }
+
+    public void setCol16(String col16) {
+        this.col16 = col16;
+    }
+
+    public String getCol17() {
+        return col17;
+    }
+
+    public void setCol17(String col17) {
+        this.col17 = col17;
+    }
+
+    public String getCol18() {
+        return col18;
+    }
+
+    public void setCol18(String col18) {
+        this.col18 = col18;
+    }
+
+    public String getCol19() {
+        return col19;
+    }
+
+    public void setCol19(String col19) {
+        this.col19 = col19;
+    }
+
+    public String getCol20() {
+        return col20;
+    }
+
+    public void setCol20(String col20) {
+        this.col20 = col20;
+    }
+}
--- report/src/main/java/com/zh/Entity/TestClass.java
+++
@@ -1,7 +0,0 @@
-package com.zh.Entity;
-
-public class TestClass {
-
-    private String str;
-    
-}
