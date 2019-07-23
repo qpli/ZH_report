80a9fc5a60492007e20bcab9cdd599a9f8797a4d
lqp <lipeixida@outlook.com>
2019-07-23 11:01:45 +0800

    regist

--- report/src/main/java/com/zh/ReportApplication.java
+++ report/src/main/java/com/zh/ReportApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReportApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReportApplication.class, args);
     }
+
 
 }
